1c553355ab8bbfd157bf9616dfc86dfe43ae5a44
vhsvhs <victorhansonsmith@gmail.com>
2013-01-23 23:44:00 -0800

    I added pymol_viz

---
+++ pymol_viz.py
@@ -0,0 +1,59 @@
+from config import *
+from anccomp_tools import *
+
+PYMOL = "/Applications/MacPyMOL.app/Contents/MacOS/MacPyMOL"
+
+
+def do_pymol_viz(ap, data, seedseq):
+    pdb_path = ap.getOptionalArg("--pdb_path")
+    start_site = int(ap.getOptionalArg("--pdb_start_site"))
+    if start_site == False:
+        print "\n. You need to specify --pdb_start_site"
+        exit()
+        
+    print "\n. Plotting H data on the PDB file", pdb_path
+    
+    print data
+    print seedseq
+    
+    outlines = ""
+    outlines += "cmd.load(\"" + os.getcwd() + "/" + pdb_path + "\")\n"
+    pdb_site = 1
+    ref_site = start_site
+    
+    maxh = data[1]
+    minh = data[1]
+    for site in data:
+        if maxh < data[site]:
+            maxh = data[site]
+        if minh > data[site]:
+            minh = data[site]
+    
+    while (ref_site < seedseq.__len__() ):
+        if seedseq[ref_site] != "-":
+            h = data[ref_site]
+            if h > 0:
+                red = 1.0 - (h / maxh) * 0.8 + 0.2
+                green = 1.0 - (h / maxh) * 0.8 + 0.2
+                blue = 0.2
+            if h < 0:
+                blue = 1.0 - (h / minh) * 0.8 + 0.2
+                green = 1.0 - (h / minh) * 0.8 + 0.2
+                red = 0.2
+            if h == 0:
+                blue = 0.9
+                green = 0.9
+                red = 0.9
+            print h, blue, green, red
+            this_color = "[" + red.__str__() + "," + green.__str__() + "," + blue.__str__() + "]"
+            outlines += "cmd.set_color('color" + pdb_site.__str__() + "'," + this_color + ")\n"
+            outlines += "cmd.color(\"color" + pdb_site.__str__() + "\", \"resi " + pdb_site.__str__() + "\")\n"
+            pdb_site += 1
+        ref_site += 1
+            
+    scriptpath = "pymol_script.p"
+    fout = open(scriptpath, "w")
+    fout.write(outlines)
+    fout.close()
+    
+    #os.system(PYMOL + " " + scriptpath)
